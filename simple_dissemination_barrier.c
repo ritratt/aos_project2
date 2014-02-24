a3316c4a521eb75c46dee1b02c7ca68d31b85ce2
Aabha Biyani <aabha.biyani@gmail.com>
2014-02-23 19:45:15 -0500

    Modified the mpi dissemination barrier to take the number of processors from -np option

--- simple_dissemination_barrier.c
+++ simple_dissemination_barrier.c
@@ -3,17 +3,16 @@
 #include <mpi.h>
 #include <math.h>
 
-#define NUM_THREADS 	6
-#define ROUNDS		3
-
 int rank, size, sense, parity, tag;
 MPI_Status status;
+int NUM_THREADS, ROUNDS;
 
 void dissemination_barrier_init()
 {
-	printf("In init()\n");
 	sense = 1;
 	tag = 1;
+	NUM_THREADS = size;
+	ROUNDS = ceil(log(NUM_THREADS)/log(2));
 }
 
 void dissemination_barrier()
@@ -23,11 +22,9 @@ void dissemination_barrier()
 	{
 		int mysaypartner = (rank + (int) pow((double)2, (double)round)) % NUM_THREADS;
 		int mylistenpartner = (rank - (int) pow((double)2, (double)round) + NUM_THREADS) % NUM_THREADS;
-		printf("rank = %d round = %d mysaypartner = %d \t mylistenpartner = %d\n", rank, round, mysaypartner, mylistenpartner);
 		MPI_Send(&sense, 1, MPI_INT, mysaypartner, tag, MPI_COMM_WORLD);
 		int recd_sense;
                 MPI_Recv(&recd_sense, 1, MPI_INT, mylistenpartner, tag, MPI_COMM_WORLD, &status);
-		printf("rank: %d round= %d listenpartner= %d recd_sense= %d\n", rank,  round, mylistenpartner, recd_sense);
 	}
 	/*Toggle the sense*/
 	sense = (sense + 1) % 2;
@@ -35,25 +32,24 @@ void dissemination_barrier()
 
 int main(int argc, char **argv)
 {
-	dissemination_barrier_init();
 	clock_t start = clock();
 
   	MPI_Init(&argc, &argv);
   	MPI_Comm_size(MPI_COMM_WORLD, &size);
   	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	printf("Initializing rank %d\n", rank);	
-//	dissemination_barrier_init();	
+	dissemination_barrier_init();	
 	int i;
 	for(i = 0 ; i < 10; i++)
 	{
 	        printf("Thread No. %d sleeping\n", rank);
 	        sleep(rank);
 	        printf("%d awake\n", rank);
-//		dissemination_barrier();
+		dissemination_barrier();
 		MPI_Barrier(MPI_COMM_WORLD);
 		printf("Barrier %d completed for %d\n", i+1, rank);
 	}
-  	printf("Finalizing rank %d\n", rank);
+ 	printf("Finalizing rank %d\n", rank);
   	MPI_Finalize();
 	clock_t end = clock();
 	float seconds = (float)(end - start);
